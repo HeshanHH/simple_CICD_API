5f67d909f0205834dcf65ed169a9fa1b401e466a
HeshanHH <heshanwijerathna01@gmail.com>
2021-04-03 19:57:43 +0530

    Initial Commit

---
+++ src/SimpleAPI/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        //private readonly ILogger<WeatherForecastController> _logger;
+
+        //public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        //{
+        //    _logger = logger;
+        //}
+        public WeatherForecastController()
+        {
+
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get( int id)
+        {
+            //var rng = new Random();
+            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //{
+            //    Date = DateTime.Now.AddDays(index),
+            //    TemperatureC = rng.Next(-20, 55),
+            //    Summary = Summaries[rng.Next(Summaries.Length)]
+            //})
+            //.ToArray();
+
+            return "abc";
+
+        }
+    }
+}
---
+++ test/SimpleAPI.Test/UnitTest.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers; // add namespace here
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest
+    {
+        WeatherForecastController controller = new WeatherForecastController(); // create an object of the WeatherForecastController
+
+        [Fact]
+        public void GetReturnWeatherData()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("abc", returnValue.Value); // grab the value
+        }
+
+
+        //[Fact]
+        //public void Test()
+        //{
+
+        //}
+    }
+}
